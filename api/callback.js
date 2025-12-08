ab4e81842828db647ccbab7c6d24da7e31cc5c29
kagami2411 <miura.8924@gmail.com>
2025-12-09 04:06:27 +0900

    Update callback.js

--- api/callback.js
+++ api/callback.js
@@ -1,10 +1,1 @@
-export default function handler(req, res) {
-  if (req.method === 'POST') {
-    console.log("LINE Webhook Received!");
 
-    res.status(200).send("OK"); // ★これが超重要★
-
-  } else {
-    res.status(405).send("Method Not Allowed");
-  }
-}
